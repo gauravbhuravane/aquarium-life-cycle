26fb1f0a3924825fa2707727320db44287f662f0
gauravbhuravane <bhuravanegaurav123@gmail.com>
2025-07-18 09:29:40 +0530

    Create debug.php

---
+++ debug.php
@@ -0,0 +1,6 @@
+<?php
+echo "DB_HOST: " . getenv("DB_HOST") . "<br>";
+echo "DB_USER: " . getenv("DB_USER") . "<br>";
+echo "DB_PASSWORD: " . getenv("DB_PASSWORD") . "<br>";
+echo "DB_NAME: " . getenv("DB_NAME") . "<br>";
+?>
