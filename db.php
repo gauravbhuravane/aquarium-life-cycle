95a8e72e9d72385af4586a5f74ecfa2b9847bb6a
gauravbhuravane <bhuravanegaurav123@gmail.com>
2025-07-18 08:21:16 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 
-  $con = mysqli_connect("localhost","root","","fish_info");
+  $con = mysqli_connect("sql109.infinityfree.com","if0_39495578","Gau1907bhu","if0_39495578_fish_info");
 
   if(!$con){
     die("Connection Error");
